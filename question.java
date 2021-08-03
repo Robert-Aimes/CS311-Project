f22419ec55ad6aeaf43013251521bdf29d9a69b1
Robert-Aimes <65087616+Robert-Aimes@users.noreply.github.com>
2021-08-03 17:13:29 -0500

    Create question.java

---
+++ question.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Question {
+    String prompt;
+
+
+    public Question(String prompt){
+        this.prompt = prompt;
+
+    }
+
+    public String getPrompt(){
+        return prompt;
+    }
+
+
+}
